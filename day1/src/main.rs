a376487b8217fc465db63770238f27659c29dae1
Rheinard Korf <rheinard@thekorfs.com>
2019-12-02 15:16:33 +1100

    Simpler without floats.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,32 +2,23 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, Result};
 
 fn main() -> Result<()> {
-    let mut count: f32 = 0.0;
+    let mut count: i32 = 0;
 
     let file = File::open("file.txt")?;
     for line in BufReader::new(file).lines() {
-        let value: f32 = line?.parse().unwrap();
-
-        // part 1
-        // count = count + ((value/3.0).floor() - 2.0);
-
-        count = count + module_consumption(value, 0.0);
+        let value: i32 = line?.parse().unwrap();
+        count = count + module_consumption(value, 0);
     }
     println!("{}", count);
     Ok(())
 }
 
-// part 2
-fn module_consumption(fuel: f32, count: f32) -> f32 {
-    if fuel < 0.0 {
+fn module_consumption(fuel: i32, count: i32) -> i32 {
+    if fuel < 0 {
         return count - fuel;
     }
 
-    if fuel == 0.0 {
-        return count;
-    }
-
-    let calculated_fuel = (fuel/3.0).floor() - 2.0;
+    let calculated_fuel = fuel / 3 - 2;
     let sum = count + calculated_fuel;
 
     return module_consumption(calculated_fuel, sum);
