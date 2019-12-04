935a164010b7f8a9b2c3bd26f722e0d9777e575c
Rheinard Korf <rheinard@thekorfs.com>
2019-12-05 02:31:11 +1100

    Added comments.

--- day4/main.rs
+++ day4/main.rs
@@ -20,12 +20,24 @@ fn is_valid(pass: u32) -> bool {
     let mut prev = 0;
     let mut res = pass;
 
+    // Loop through each digit going from left to right.
     for i in (0..6).rev() {
+
+        // This will create the following upon each loop:
+        // 100k 10k 1k 100 10 1
+        // These are used to isolate the most left digit for processing.
         let dec = 10_u32.pow(i);
+
+        // Get the digit.
         let dig = res / dec;
+
+        // Remove the digit and work out what remains.
         res = res - (dig*dec);
 
+        // Can be the same, but should not get smaller.
         if dig < prev { return false; }
+
+        // Test for adjacency.
         if dig == prev {
             has_adj = true;
 
@@ -39,17 +51,22 @@ fn is_valid(pass: u32) -> bool {
             adj_count = 1;
         }
 
+        // Remember the last digit processed.
         prev = dig;
     }
 
     // Part 2.
+    // If we reach the end of the loop we never get to test this.
+    // So lets do it now!
     if adj_count == 2 {
         has_2 = true;
     }
 
     // Part 1.
+    // Has at least one adjecent number.
     // return has_adj;
 
     // Part 2.
+    // Must have at least 1 adjacency pair.
     return has_adj && has_2;
 }
